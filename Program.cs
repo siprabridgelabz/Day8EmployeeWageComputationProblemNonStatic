57545c56b48fb91f4fb1985cdb5e0eb00ef1183b
sipra <sipramishra085@gmail.com>
2023-03-18 04:06:55 +0530

    Added program file

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Day8EmployeeWageComputationProblemNonStatic;
+
+namespace EmployeeWageComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to employee wage computation problem");
+
+
+
+            // EmployeeAttendence attendence = new EmployeeAttendence();
+
+            // EmployeeAttendence.EmpAttendance();
+
+            //EmployeeCheck.EmployeeWage();
+            //EmployeeCheck check = new EmployeeCheck();
+
+            // PartTimeEmployee.PartTimeEmployeeWage();
+            // PartTimeEmployee employee = new PartTimeEmployee();
+
+            // EmployeeWageSwitcCase.EmployeeWageSwitchCaseStatement();
+            // EmployeeWageSwitcCase switcCase = new EmployeeWageSwitcCase();
+
+            // WagesForMonth.WagesforaMonth();
+            // WagesForMonth month = new WagesForMonth();
+
+
+            // EmployeeWageCondition.EmployeeWageConditionForWorkingHours();
+          //  EmployeeWageCondition condition = new EmployeeWageCondition();
+
+
+        }
+    }
+}
